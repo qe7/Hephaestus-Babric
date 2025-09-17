2023572ec507952717c82ac85cc72f41f929148c
shae <89613516+qe7@users.noreply.github.com>
2025-09-17 22:41:13 +0100

    Add module list display to HUD module

--- src/main/java/io/github/qe7/features/modules/render/HUDModule.java
+++ src/main/java/io/github/qe7/features/modules/render/HUDModule.java
@@ -5,14 +5,21 @@ import io.github.qe7.core.bus.Listener;
 import io.github.qe7.core.bus.SubscribeEvent;
 import io.github.qe7.core.feature.module.AbstractModule;
 import io.github.qe7.core.feature.module.ModuleCategory;
+import io.github.qe7.core.feature.module.ModuleManager;
 import io.github.qe7.core.feature.module.settings.impl.BooleanSetting;
+import io.github.qe7.core.manager.ManagerFactory;
 import io.github.qe7.events.ScreenEvent;
 import net.minecraft.src.FontRenderer;
 import net.minecraft.src.ScaledResolution;
 
+import java.util.Comparator;
+import java.util.List;
+
 public final class HUDModule extends AbstractModule {
 
     private final BooleanSetting displayWatermark = new BooleanSetting("Display Watermark", true);
+
+    private final BooleanSetting displayModuleList = new BooleanSetting("Display Module List", true);
 
     public HUDModule() {
         super("HUD", "Displays information related to the client", ModuleCategory.RENDER);
@@ -29,6 +36,24 @@ public final class HUDModule extends AbstractModule {
 
         if (this.displayWatermark.getValue()) {
             fontRenderer.drawStringWithShadow(HephaestusMod.INSTANCE.getName() + " " + HephaestusMod.INSTANCE.getVersion(), 2, 2, -1);
+        }
+
+        if (this.displayModuleList.getValue()) {
+            final List<AbstractModule> moduleList = ManagerFactory.get(ModuleManager.class).getEnabledModules();
+
+            moduleList.sort(Comparator.comparingInt(abstractModule -> -fontRenderer.getStringWidth(abstractModule.getName() + (abstractModule.getSuffix() != null ? " " + abstractModule.getSuffix() : ""))));
+
+            int offset = 2;
+            for (AbstractModule abstractModule : moduleList) {
+                if (!abstractModule.isDrawn()) {
+                    continue;
+                }
+
+                final String display = abstractModule.getName() + (abstractModule.getSuffix() != null ? " " + "§7" + abstractModule.getSuffix() : "");
+
+                fontRenderer.drawStringWithShadow(display, event.getScaledResolution().getScaledWidth() - fontRenderer.getStringWidth(display) - 2, offset, -1);
+                offset += 9;
+            }
         }
     };
 }
